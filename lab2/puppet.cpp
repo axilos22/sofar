02b9bc3a0aa5b46ee44cfb3047a79981cebbec05
axilos22 <zenos_22@hotmail.com>
2016-04-20 10:09:10 +0200

    update puppet
    -Correction of limbs
    -correction of joint position
    -check what does the limbs public

--- lab2/puppet.cpp
+++ lab2/puppet.cpp
@@ -7,7 +7,7 @@
 
 //Cpp
 #include <iostream>
-#include <sstream.h>
+#include <string>
 #include <vector>
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,7 +16,7 @@
 #include "ros/ros.h"
 
 //ROS msgs
-#include <std_msg/Int16.h>
+#include <std_msgs/Int16.h>
 #include <sensor_msgs/JointState.h>
 #include <baxter_core_msgs/JointCommand.h>
 
@@ -27,20 +27,25 @@
 
 int _freq = 1; //1Hz frequency
 bool _isRightArmPuppet, _isLeftArmPuppet;
-std::vector<std::string> _jointNameList = {"left_s0"};
+std::vector<std::string> _jointNameList ;
+
 sensor_msgs::JointState _joints,_jointsP;
+ros::Publisher jointPub;
 
 /**
   \fn void getSensorPos(joint_state)
   \brief This callback stores into the local variable, the updated joint list.
   */
 void jointStateCallback(sensor_msgs::JointState joint_state) {
-    _joints.clear();
+    //ROS_INFO("jointStateCallback");
+    _joints.name.clear();
+    _joints.position.clear();
+    _joints.velocity.clear();
     //store all the joint state published into local variable
-    for(int i =0;i<joint_state.size();i++) {
+    for(int i =0;i<joint_state.name.size();i++) {
         _joints.name.push_back(joint_state.name[i]);
         _joints.position.push_back(joint_state.position[i]);
-        _joints.position.push_back(joint_state.velocity[i]);
+        _joints.velocity.push_back(joint_state.velocity[i]);
     }
     //Here my _joint variable is up to date
 }
@@ -48,11 +53,36 @@ void jointStateCallback(sensor_msgs::JointState joint_state) {
  * @brief performProcessing perform the change of sign to the new joint state
  */
 void performProcessing() {
-	_jointsP = _joints;
+    baxter_core_msgs::JointCommand msg_command;
+    std::string mySide = "right";
+    _jointsP.name.clear();
+    _jointsP.position.clear();
+    _jointsP.velocity.clear();
+    for(int i =0;i<_joints.name.size();i++) {
+        std::size_t found = _joints.name[i].find(mySide);
+         if (found!=std::string::npos) {
+             ROS_INFO("i found %s in %s",mySide.c_str(),_joints.name[i].c_str());
+              _jointsP.name.push_back(_joints.name[i]);
+              _jointsP.position.push_back(_joints.position[i]);
+              _jointsP.velocity.push_back(_joints.velocity[i]);
+         }
+    }
+    std::string publishSide = "left";
+    for(int h=0; h<_jointsP.name.size();h++) {
+        ROS_INFO("before %s",_jointsP.name[h].c_str());
+        _jointsP.name[h].replace(0,5,publishSide);
+        ROS_INFO("after %s",_jointsP.name[h].c_str());
+    }
+    msg_command.mode = msg_command.POSITION_MODE;
+    for(int j=0;j<_jointsP.name.size();j++) {
+        msg_command.names.push_back(_jointsP.name[j]);
+        msg_command.command.push_back( _jointsP.position[j]);
+    }
+    jointPub.publish(msg_command);
 }
 /**
  * @brief publish the new joint position to the puppeted arm
- */ 
+ */
 void setSlavePos() {
 
 }
@@ -65,27 +95,28 @@ int main (int argc, char** argv)
   ROS_INFO("mirror process connected to roscore");
   ros::NodeHandle nh_("~");//ROS Handler - local namespace.
 
+
+  _jointNameList.push_back("left_s0");
   nh_.param("righArmPuppet",_isRightArmPuppet,DEFAULT_ARM_RIGHT);
   nh_.param("leftArmPuppet",_isLeftArmPuppet,DEFAULT_ARM_LEFT);
-  
+
   if(_isRightArmPuppet) {
-	  ROS_INFO("Puppet arm is the right");
+      ROS_INFO("Puppet arm is the right");
   } else {
-	  ROS_INFO("Puppet arm is the left");
+      ROS_INFO("Puppet arm is the left");
   }
-  //declaring subscriber 
-  ros::Subscriber jointsSubsciber = nh_.subscribe("jointState",DEFAULT_Q_SIZE,jointStateCallback);
-  //Perform processing to invert some joints
-  performProcessing();
+  //declaring subscriber
+  ros::Subscriber jointsSubsciber = nh_.subscribe("/robot/joint_states",DEFAULT_Q_SIZE,jointStateCallback);
+
   //declaring publisher
-  ros::Publisher jointPub = nh_.advertise<sensor_msgs::JointState>("sensorPublisher",5);
-	ros::Rate rate(_freq);
-    while (ros::ok()) {   
-    	//Subscribing to the master arm joint state -- implicit
-    	//Publishing into the slave arm joint state
-    	performProcessing();
-    	jointPub.publish(_jointsP);
-    	
+  jointPub = nh_.advertise<baxter_core_msgs::JointCommand>("/robot/limbs/left/joint_command",5);
+    ros::Rate rate(_freq);
+    while (ros::ok()) {
+        //Subscribing to the master arm joint state -- implicit
+        //Publishing into the slave arm joint state
+        //Perform processing to invert some joints
+        performProcessing();
+        //jointPub.publish(_jointsP);
         ros::spinOnce();
         rate.sleep();
     }
