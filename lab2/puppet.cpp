91a7402e52971e0466ee01fe67ef30a9406b8c69
axilos22 <zenos_22@hotmail.com>
2016-05-03 09:29:38 +0200

    Update puppet.cpp
    -Added limb selection possibility
    -Added position or velocity control
    -Code cleanup
    -Additional comments

--- lab2/puppet.cpp
+++ lab2/puppet.cpp
@@ -2,7 +2,7 @@
 \file puppet.cpp
 \brief puppet.cpp creates a puppet nodes which mimic the movement of one arm to another
 \author  AJ & BG
-\date 20/04/2016
+\date 03/05/2016
 */
 
 //Cpp
@@ -20,28 +20,33 @@
 #include <sensor_msgs/JointState.h>
 #include <baxter_core_msgs/JointCommand.h>
 
+//DEBUG
+#define VERBOSE 0
+#define DEFAULT_Q_SIZE 10 //default queue size
+#define CONTROL_POSITION 1 //default control mode is 1
+#define CONTROL_VELOCITY 0
 //By default we puppet the right arm to the left
-#define CONTROLLING_RIGHT 0
-#define CONTROLLING_LEFT 1
-#define DEFAULT_Q_SIZE 10
-
-int _freq = 1; //1Hz frequency
+const std::string _defaultArm = "left",
+    _nodeName = "puppet_node",
+    _jointStateTopic = "/robot/joint_states";
+std::string _masterArm,_slaveArm;
+const unsigned int _freq = 10; //10Hz frequency
+int _controlMode;
 std::vector<std::string> _jointNameList ;
 sensor_msgs::JointState _joints,_jointsP;
 ros::Publisher jointPub;
-unsigned int _armControlled = CONTROLLING_LEFT;
 
 //function for scope
 void jointStateCallback(sensor_msgs::JointState joint_state);
 void performProcessing();
 
-
 /**
-  \fn void getSensorPos(joint_state)
-  \brief This callback stores into the local variable, the updated joint list.
-  */
+ * @brief jointStateCallback is executed when the state of joints are published.
+ * @param joint_state the joint state message of the robot.
+ */
 void jointStateCallback(sensor_msgs::JointState joint_state) {
     //ROS_INFO("jointStateCallback");
+    //cleaning last joint state
     _joints.name.clear();
     _joints.position.clear();
     _joints.velocity.clear();
@@ -51,64 +56,114 @@ void jointStateCallback(sensor_msgs::JointState joint_state) {
         _joints.position.push_back(joint_state.position[i]);
         _joints.velocity.push_back(joint_state.velocity[i]);
     }
-    //Here my _joint variable is up to date
-    //We can launch the processing to transmit to the other arm.
     performProcessing();
 }
 /**
- * @brief performProcessing perform the change of sign to the new joint state
- * perform the conversion from joint state message to joint command message
+ * @brief performProcessing 1)perform the change of sign to the new joint state (TODO)
+ *                          2)perform the conversion from joint state message to joint command message
  */
 void performProcessing() {
     baxter_core_msgs::JointCommand msg_command;
-    std::string mySide = "right";
+//    std::string _masterArm = "right";
     _jointsP.name.clear();
     _jointsP.position.clear();
     _jointsP.velocity.clear();
     for(int i =0;i<_joints.name.size();i++) {
-        std::size_t found = _joints.name[i].find(mySide);
+        std::size_t found = _joints.name[i].find(_masterArm);
         if (found!=std::string::npos) {
-            ROS_INFO("i found %s in %s",mySide.c_str(),_joints.name[i].c_str());
+            ROS_INFO("i found %s in %s",_masterArm.c_str(),_joints.name[i].c_str());
             _jointsP.name.push_back(_joints.name[i]);
             _jointsP.position.push_back(_joints.position[i]);
             _jointsP.velocity.push_back(_joints.velocity[i]);
         }
     }
-    std::string publishSide = "left";
+//    std::string publishSide = "left";
     for(int h=0; h<_jointsP.name.size();h++) {
         ROS_INFO("before %s",_jointsP.name[h].c_str());
-        _jointsP.name[h].replace(0,5,publishSide);
+        _jointsP.name[h].replace(0,5,_slaveArm);
         ROS_INFO("after %s",_jointsP.name[h].c_str());
     }
-    msg_command.mode = msg_command.POSITION_MODE;
+    if(_controlMode==CONTROL_POSITION) {
+        msg_command.mode = msg_command.POSITION_MODE;
+    } else {
+        msg_command.mode = msg_command.VELOCITY_MODE;
+    }
+
     for(int j=0;j<_jointsP.name.size();j++) {
         msg_command.names.push_back(_jointsP.name[j]);
-        msg_command.command.push_back( _jointsP.position[j]);
+        if(_controlMode==CONTROL_POSITION)
+            msg_command.command.push_back(_jointsP.position[j]);
+        else
+            msg_command.command.push_back(_jointsP.velocity[j]);
     }
     jointPub.publish(msg_command);
 }
-
+/**
+ * @brief fillJointName fill the joint name list with all the joints.
+ */
+void fillJointName(std::string armSide) {
+    _jointNameList.clear();
+    _jointNameList.push_back(armSide+"_e0");
+    _jointNameList.push_back(armSide+"_e1");
+    _jointNameList.push_back(armSide+"_s0");
+    _jointNameList.push_back(armSide+"_s1");
+    _jointNameList.push_back(armSide+"_w0");
+    _jointNameList.push_back(armSide+"_w1");
+    _jointNameList.push_back(armSide+"_w2");
+}
+/**
+ * @brief main
+ * @param argc
+ * @param argv
+ * @return
+ */
 int main (int argc, char** argv)
 {
     //ROS Initialization
-    ros::init(argc, argv, "puppet_node");
-    ROS_INFO("mirror process connected to roscore");
-    ros::NodeHandle nh_("~");//ROS Handler all namespaces
-    _jointNameList.push_back("left_s0");
-    //0 means we control right, 1 left
-//    nh_.param("armControlled",_armControlled,CONTROLLING_LEFT);
-//    ROS_INFO("The arm controlled is %d",_armControlled);
+    ros::init(argc, argv, _nodeName);
+    ROS_INFO("@%s connected to roscore",_nodeName.c_str());
+    ros::NodeHandle nh_("~");//ROS Handler absolute & relative paths
+    //Side of the arm controlled
+    std::string armControlled;
+    nh_.param<std::string>("armControlled",armControlled,_defaultArm);
+    //Controlling in position or velocity
+    nh_.param<int>("controlMode",_controlMode,CONTROL_POSITION);
+
+    //User input verification
+    if(armControlled.compare("left")==0||armControlled.compare("right")==0) {
+        ROS_ERROR("@%s Invalid arm side provided: %s",_nodeName.c_str(),armControlled.c_str());
+        return -1;
+    } else {
+        ROS_INFO("The arm controlled is %s",armControlled.c_str());
+        if(armControlled.compare("left")==0) {
+            _masterArm="right";
+            _slaveArm="left";
+        } else {
+            _masterArm="left";
+            _slaveArm="right";
+        }
+    }
+    if(_controlMode!=CONTROL_POSITION||_controlMode!=CONTROL_VELOCITY) {
+        ROS_ERROR("@%s Invalid control mode provided: %d",_nodeName.c_str(),_controlMode);
+        return -1;
+    } else {
+        if (_controlMode==CONTROL_POSITION)
+            ROS_INFO("Robot controlled in position");
+        if (_controlMode==CONTROL_VELOCITY)
+            ROS_INFO("Robot controlled in velocity");
+    }
+    fillJointName(_masterArm);
 
     //declaring subscriber
-    ros::Subscriber jointsSubsciber = nh_.subscribe("/robot/joint_states",DEFAULT_Q_SIZE,jointStateCallback);
+    ros::Subscriber jointsSubsciber = nh_.subscribe(_jointStateTopic,DEFAULT_Q_SIZE,jointStateCallback);
 
     //declaring publisher
-    jointPub = nh_.advertise<baxter_core_msgs::JointCommand>("/robot/limbs/left/joint_command",5);
+    std::string jointCommandTopic = "/robot/limbs/"+_slaveArm+"/joint_command";
+    jointPub = nh_.advertise<baxter_core_msgs::JointCommand>(jointCommandTopic,5);
     ros::Rate rate(_freq);
     while (ros::ok()) {
         ros::spinOnce();
         rate.sleep();
     }
-
-    ROS_INFO("ROS-Node Terminated\n");
+    ROS_INFO("@%s Node terminated\n",_nodeName.c_str());
 }
